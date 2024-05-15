2a35c9e45d72ae4abd73493b855540937d17c088
Elliotte Wideman <ciaontop@gmail.com>
2024-05-15 14:09:49 -0400

    Batch file updates to run package

--- run_javafx.bat
+++ run_javafx.bat
@@ -22,8 +22,8 @@ if %choice%==1 (
 )
 
 :run_all_programs
-for %%F in (*.java) do (
-    if "%%F" neq "MyJavaFX.java" (
+for %%F in (src\chpt\14\programs\*.java) do (
+    if "%%~nF" neq "MyJavaFX" (
         echo Running %%~nF
         javac --module-path "C:\Program Files\Java\javafx-sdk-22.0.1\lib" --add-modules=javafx.controls "%%F"
         java --module-path "C:\Program Files\Java\javafx-sdk-22.0.1\lib" --add-modules=javafx.controls "%%~nF"
@@ -35,52 +35,9 @@ goto start
 :run_specific_program
 cls
 echo List of programs:
-dir /b *.java
+dir /b src\chpt\14\programs\*.java
 set /p program="Enter the program name (without extension): "
-javac --module-path "C:\Program Files\Java\javafx-sdk-22.0.1\lib" --add-modules=javafx.controls "%program%.java"
-java --module-path "C:\Program Files\Java\javafx-sdk-22.0.1\lib" --add-modules=javafx.controls "%program%"
-pause
-goto start
-@echo off
-
-:start
-cls
-echo Choose an option:
-echo 1. Run all programs
-echo 2. Run a specific program
-echo 3. Exit
-
-set /p choice="Enter your choice: "
-
-if %choice%==1 (
-    call :run_all_programs
-) else if %choice%==2 (
-    call :run_specific_program
-) else if %choice%==3 (
-    exit
-) else (
-    echo Invalid choice. Please try again.
-    pause
-    goto start
-)
-
-:run_all_programs
-for %%F in (*.java) do (
-    if "%%F" neq "MyJavaFX.java" (
-        echo Running %%~nF
-        javac --module-path "C:\Program Files\Java\javafx-sdk-22.0.1\lib" --add-modules=javafx.controls "%%F"
-        java --module-path "C:\Program Files\Java\javafx-sdk-22.0.1\lib" --add-modules=javafx.controls "%%~nF"
-        pause
-    )
-)
-goto start
-
-:run_specific_program
-cls
-echo List of programs:
-dir /b *.java
-set /p program="Enter the program name (without extension): "
-javac --module-path "C:\Program Files\Java\javafx-sdk-22.0.1\lib" --add-modules=javafx.controls "%program%.java"
-java --module-path "C:\Program Files\Java\javafx-sdk-22.0.1\lib" --add-modules=javafx.controls "%program%"
+javac --module-path "C:\Program Files\Java\javafx-sdk-22.0.1\lib" --add-modules=javafx.controls "src\chpt\14\programs\%program%.java"
+java --module-path "C:\Program Files\Java\javafx-sdk-22.0.1\lib" --add-modules=javafx.controls "src\chpt\14\programs\%program%"
 pause
 goto start
